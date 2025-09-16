32ea813f63fe2af0fb3fe6ccc1cf0a037a3eb4ee
Michael <228615298+mzhao2025@users.noreply.github.com>
2025-09-16 13:29:50 -0700

    09/16/25

---
+++ Unit 0 Student Profiles Lab/Student.java
@@ -0,0 +1,69 @@
+public class Student {
+    private String name;
+    private String id;
+    private int grade;
+
+    public Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+        this.id = generateId();
+    }
+
+    public Student(String name) {
+        this.name = name;
+        this.grade = 10;
+        this.id = generateId();
+    }
+
+    public String generateId() {
+        String id = "";
+
+        for (int i = 0; i < 3; i++) {
+            int digit = (int)(Math.random() * 8) + 1; // 1–8
+            id += digit;
+        }
+
+        id += "-";
+    
+        for (int i = 0; i < 4; i++) {
+            int digit = (int)(Math.random() * 10); // 0–9
+            id += digit;
+        }
+
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public String toString() {
+        return name + " is a " + grade + "th grader. Their id is " + id;
+    }
+
+    public boolean equals(Student other) {
+        return (this.id == other.id) && (this.name == other.name) && (this.grade == other.grade);
+    }
+
+
+}
---
+++ Unit 0 Student Profiles Lab/studentTester.java
@@ -0,0 +1,25 @@
+public class studentTester {
+    public static void main(String[] args) {
+        Student student1 = new Student("Alice", 12);
+        Student student2 = new Student("Bob", 10);
+        Student student3 = new Student("Calvin", 12);
+        Student student4 = new Student("Dennis", 10);
+
+        System.out.println(student1.toString());
+        System.out.println(student2.toString());
+        System.out.println(student3.toString());
+        System.out.println(student4.toString());
+
+        student1.setGrade(13);
+        student1.setGrade(11);
+        student1.setGrade(13);
+        student1.setGrade(11);
+
+        System.out.println(student1.toString());
+        System.out.println(student2.toString());
+        System.out.println(student3.toString());
+        System.out.println(student4.toString());
+
+        System.out.println(student1.equals(student2));
+    }
+}
